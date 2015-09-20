32da791aebb927171ae86d7143b4086626c107fb
Douglas Raith <draith2001@gmail.com>
2015-09-20 10:38:46 +0100

    Fixed URL injection and backlog upload.

--- logtemp.sh
+++ logtemp.sh
@@ -1,55 +1,56 @@
 #!/bin/sh
-FILENAME=$(date +"%H.%M")
-LOGDIR='/home/pi/usbdrv/templog'
 SOURCE='/sys/bus/w1/devices/28-000007099503/w1_slave'
 TEMPSTRING=$(cat $SOURCE)
+TEMPVAL=${TEMPSTRING##*=}
+TIMESTAMP=$(date -u "+%Y-%m-%d%%20%H:%M:00")
+
+LOGDIR='/home/pi/usbdrv/templog'
 BACKLOGFILE='/home/pi/pitemp/backlog.txt'
 NEWBACKLOG='/var/ram/backlog.txt'
-BACKLOGLOGFILE='/home/pi/pitemp/backlog.log'
-TEMPVAL=${TEMPSTRING##*=}
 
 # Save reading in local file.
+FILENAME=$(date +"%H.%M")
 mv $LOGDIR/$FILENAME $LOGDIR/yesterday/$FILENAME
 echo $TEMPVAL > $LOGDIR/$FILENAME
 chmod a+w $LOGDIR/$FILENAME
 
 # Send reading to web database
-# REQUIRED ENVIRONMENT VARIABLES URL1 and URL2 to compile URL for uploading.
-# e.g. full URL = "http://www.myurl.com/upload.php?time=$TIMESTAMP\&temp=$TEMPVAL"
-TIMESTAMP=$(date -u "+%Y-%m-%d%%20%H:%M:00")
-RESPONSE=$(curl http://www.mekeke.co.uk/pitemp/logtemp.php?id=mypicam\&timestamp=$TIMESTAMP\&reading=$TEMPVAL)
+RESPONSE=$(curl $URL1$TIMESTAMP$URL2$TEMPVAL)
+# echo "Sent:" $URL1$TIMESTAMP$URL2$TEMPVAL
+# echo "Response:" $RESPONSE
 
 if [ "$RESPONSE" != "OK" ]
 then
   # FAILED: Append date and reading to backlog file
   mail -s "temp log failed" draith2001@gmail.com
   echo "$TIMESTAMP $TEMPVAL" >> $BACKLOGFILE
+  echo "Upload failed:" $RESPONSE
 else
   # Success. Now upload any backlog.
   while read line; do
-    words=($line)
-    TIMESTAMP=${words[0]}
-    TEMPVAL=${words[1]}
+    TIMESTAMP=${line%% *}
+    TEMPVAL=${line##* }
     if [ "$RESPONSE" != "OK" ]
     then
       echo "$TIMESTAMP $TEMPVAL" >> $NEWBACKLOG
     else
-      echo "Uploading backlog" $TIMESTAMP $TEMPVAL >> $BACKLOGLOGFILE
-      RESPONSE=$(curl http://www.mekeke.co.uk/pitemp/logtemp.php?id=mypicam\&timestamp=$TIMESTAMP\&reading=$TEMPVAL)
+      echo "Uploading backlog" $TIMESTAMP $TEMPVAL
+       RESPONSE=$(curl $URL1$TIMESTAMP$URL2$TEMPVAL)
       if [ "$RESPONSE" != "OK" ]
       then
-        echo "RESPONSE =" $RESPONSE >> $BACKLOGLOGFILE
+        echo "RESPONSE =" $RESPONSE
         echo "$TIMESTAMP $TEMPVAL" >> $NEWBACKLOG
       else
-        echo "OK: " $TIMESTAMP $TEMPVAL >> $BACKLOGLOGFILE
+        echo "OK: " $TIMESTAMP $TEMPVAL
       fi
     fi
   done < $BACKLOGFILE
   
   if [ "$RESPONSE" != "OK" ]
   then
+    echo "Updating" $BACKLOGFILE
     mv $NEWBACKLOG $BACKLOGFILE
   else
-    echo -n "" > $BACKLOGFILE
+    echo -n > $BACKLOGFILE
   fi
 fi
